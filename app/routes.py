17bbaf93cc8a21888afa2d10369a90f5237da25b
krutika-1496 <mohitekrutika14@gmail.com>
2020-01-08 20:38:59 +0530

    third commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,30 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
+# def index():
+#     user = {'username': 'krutika'}
+#     return '''
+# <html>
+#     <head>
+#         <title>Home Page - Microblog</title>
+#     </head>
+#     <body>
+#         <h1>Hi..., ''' + user['username'] + '''!</h1>
+#     </body>
+# </html>'''
 def index():
-    return " krutika"
+    user = {'username': 'krutika'}
+    posts = [
+    
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
